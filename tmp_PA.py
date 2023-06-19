92fcb821a62caecb791cb120e7d7583736f1a6ad
PaulAdrPENET <pauladrienpenet@gmail.com>
2023-06-19 13:59:56 +0200

    Préparation des données.

---
+++ tmp_PA.py
@@ -0,0 +1,9 @@
+# Réduction des dimensions Paul-Adrien PENET
+# On calcule la matrice de corrélation :
+from main import data
+import main
+import pandas as pd
+
+#cmatrice = data.corr()
+#print(cmatrice)
+#random forest
