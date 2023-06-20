20e8a2315563c8b0629afaa5bcd508278970848a
PaulAdrPENET <pauladrienpenet@gmail.com>
2023-06-20 14:42:36 +0200

    Préparation des données.

--- tmp_PA.py
+++ tmp_PA.py
@@ -13,7 +13,7 @@ On choisit de supprimer certaines classes (classes ayant une corrélation faible
 def reduction_dim(data):
    numeric = data.select_dtypes(include=['float64', 'int64'])
    cmatrice = numeric.corr()
-   print(cmatrice)
+   #print(cmatrice)
    # On choisit de supprimer certaines classes (classes ayant une corrélation faible).
    # Ainsi que les classes non-utile.
    #plt.figure(figsize=(12, 10))
@@ -30,17 +30,17 @@ def reduction_dim(data):
    #On vérifie de nouveau la corrélation entre les classes.
    numeric = data.select_dtypes(include=['float64', 'int64'])
    cmatrice = numeric.corr()
-   print(cmatrice)
+   #print(cmatrice)
    #plt.figure(figsize=(12, 10))
    #sns.heatmap(cmatrice, annot=True, cmap='coolwarm')
    #plt.show()
-   data = data.drop("descr_cat_veh", axis=1)
+   data = data.drop("hours", axis=1)
    data = data.drop("descr_agglo", axis=1)
    data = data.drop("descr_motif_traj", axis=1)
    data = data.drop("description_intersection", axis=1)
    data = data.drop("descr_type_col", axis=1)
    data = data.drop("descr_dispo_secu", axis=1)
-   data = data.drop("descr_athmo", axis=1)
+   data = data.drop("age", axis=1)
    data = data.drop("month", axis=1)
    #On supprime également les colonnes non numériques n'apparaissant pas dans la matrice :
    data = data.drop("date", axis = 1)
@@ -48,10 +48,10 @@ def reduction_dim(data):
    data = data.drop("time", axis = 1)
    numeric = data.select_dtypes(include=['float64', 'int64'])
    cmatrice = numeric.corr()
-   print(cmatrice)
+   #print(cmatrice)
    #plt.figure(figsize=(12, 10))
    #sns.heatmap(cmatrice, annot=True, cmap='coolwarm')
-   print(data)
+   #print(data)
    #plt.show()
    # Forest test (comparer les résultats).
    return data
@@ -61,12 +61,26 @@ le temps d'execution des algorithmes nous conservons uniquement 4000 données.
 """
 def reduction_data(data):
     data_filtre = data.groupby('descr_grav').head(1000)
-    print(data_filtre)
+    #print(data_filtre)
     return data_filtre
 # Cependant les données étant regroupées par description grave, il faut les mélanger.
 
+def repartition_data(data):
+   #Dans la dataframe data_ready les données sont ordonnées à cause du groupby.
+   #On répartit les données de façon aléatoire.
+   data_unsorted = data.sample(frac=1,random_state=1).reset_index(drop=True)
+   # Holdout
+   X = data_unsorted
+   y = data['descr_grav']
+   return X,y
+
 
 data_reduc_dim = reduction_dim(data)
-print(data_reduc_dim)
+#print(data_reduc_dim)
 data_ready = reduction_data(data_reduc_dim)
 print(data_ready)
+X, y = repartition_data(data_ready)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.8, random_state=42)
+
+#Holdout:
+
