f780c0398a4f18bef4fd361fdd0beebb37097d20
PaulAdrPENET <pauladrienpenet@gmail.com>
2023-06-20 09:10:55 +0200

    Préparation des données. 7 variables seulement

--- tmp_PA.py
+++ tmp_PA.py
@@ -1,6 +1,9 @@
+import pandas as pd
+
 from Preparation import *
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestRegressor
 
 data = rewrite_data()
 
@@ -13,7 +16,7 @@ def reduction_dim(data):
     # Ainsi que les classes non-utile.
     plt.figure(figsize=(12, 10))
     sns.heatmap(cmatrice, annot=True, cmap='coolwarm')
-    plt.show()
+    #plt.show()
     data = data.drop("Num_Acc", axis=1)
     data = data.drop("num_veh", axis=1)
     data = data.drop("id_usa", axis=1)
@@ -28,18 +31,21 @@ def reduction_dim(data):
     print(cmatrice)
     plt.figure(figsize=(12, 10))
     sns.heatmap(cmatrice, annot=True, cmap='coolwarm')
-    plt.show()
+    #plt.show()
     data = data.drop("descr_cat_veh", axis=1)
     data = data.drop("descr_agglo", axis=1)
     data = data.drop("descr_motif_traj", axis=1)
     data = data.drop("description_intersection", axis=1)
     data = data.drop("descr_type_col", axis=1)
+    data = data.drop("descr_dispo_secu", axis=1)
+    data = data.drop("descr_athmo", axis=1)
+    data = data.drop("month", axis=1)
     numeric = data.select_dtypes(include=['float64', 'int64'])
     cmatrice = numeric.corr()
     print(cmatrice)
     plt.figure(figsize=(12, 10))
     sns.heatmap(cmatrice, annot=True, cmap='coolwarm')
-    plt.show()
+    #plt.show()
     # Forest test (comparer les résultats).
     return data
 #On choisit de supprimer certaines classes (classes ayant une corrélation faible).
